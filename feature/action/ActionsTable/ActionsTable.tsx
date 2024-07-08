9022c4b2ea66a0207eacbb3fac460177d8926eb1
INO <katsuki104@gmail.com>
2024-07-08 22:58:28 +0900

    tableをdaisyUIに置き換える

--- feature/action/ActionsTable/ActionsTable.tsx
+++ feature/action/ActionsTable/ActionsTable.tsx
@@ -1,42 +1,37 @@
 import type { Action } from "@/app/api/types";
-import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-} from "@/components/ui/table";
 import React from "react";
 
 const ActionsTable = ({ actions }: { actions: Action[] }) => {
 	return (
-		<div className="p-3">
-			<Table>
-				<TableHeader>
-					<TableRow>
-						<TableHead>アクション名</TableHead>
-						<TableHead>Status</TableHead>
-						<TableHead>UpdatedAt</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					{actions.map((action) => (
-						<TableRow key={action.id}>
-							<TableCell className="font-medium">{action.name}</TableCell>
-							<TableCell>{action.canBeExecutedOnOthers ? "⚪︎" : "×"}</TableCell>
-							<TableCell className="text-right">
+		<div className="overflow-x-auto">
+			<table className="table">
+				{/* head */}
+				<thead>
+					<tr>
+						<th />
+						<th>Action Name</th>
+						<th>Status</th>
+						<th>UpdatedAt</th>
+					</tr>
+				</thead>
+				<tbody>
+					{actions.map((action, index) => (
+						<tr key={action.id} className="hover">
+							<th>{index + 1}</th>
+							<td>{action.name}</td>
+							<td>{action.canBeExecutedOnOthers ? "OK" : "NG"}</td>
+							<td>
 								{new Date(action.updatedAt).toLocaleDateString("ja-JP", {
 									weekday: "long",
 									year: "numeric",
 									month: "long",
 									day: "numeric",
 								})}
-							</TableCell>
-						</TableRow>
+							</td>
+						</tr>
 					))}
-				</TableBody>
-			</Table>
+				</tbody>
+			</table>
 		</div>
 	);
 };
